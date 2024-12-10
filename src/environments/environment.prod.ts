7b3450dbd78b77871287870646bb2e0fade13664
AbhiS1892 <shri.abhishek01@gmail.com>
2024-12-10 22:19:11 +0530

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  // Add any production-specific variables here
+  apiUrl: 'https://your-production-api-url.com'  // Example of a production API URL
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  // Add any development-specific variables here
+  apiUrl: 'https://your-development-api-url.com'  // Example of a development API URL
+};
